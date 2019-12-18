7987aa4479789d114ab0fa2d44622e29d84a771d
Iam H <sumaya_khair@live.com>
2019-12-18 03:04:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,10 @@ def directors_totals(nds)
   pp nds
   result = {}
   row_index =0 
-  while row_index < nds.length 
+  while row_index < nds.length do
+    d_names = nds[row_index][:name]
+    result[d_names] = nds
+  end
   
   nil
 end
